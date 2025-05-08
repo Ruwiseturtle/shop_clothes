1a2e80e82907b3be7dd357bca79e1aac364dc9cb
Matviienko Ruslana <brauniesrus@gmail.com>
2025-05-08 12:57:02 +0300

    brends

--- src/components/Brends/Brends.js
+++ src/components/Brends/Brends.js
@@ -4,9 +4,15 @@ import './Brends.css';
 const Brends = () => {
   return (
     <div className='container-brends'>
-      sss
+      <div className="box-brends">
+        <div className="Versage">VERSAGE</div>
+        <div className="Zara">ZARA</div>
+        <div className="Gucci">GUCCI</div>
+        <div className="Prada">PRADA</div>
+        <div className="Calvin-klein">Calvin Klein</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Brends
